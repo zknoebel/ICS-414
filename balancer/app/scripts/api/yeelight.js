76dcc12935dc01e1c5a07dc629f5cd46bd886874
zknoebel <zacknoebel@hawaii.edu>
2019-04-22 05:58:51 -1000

    WIP

--- balancer/app/scripts/api/yeelight.js
+++ balancer/app/scripts/api/yeelight.js
@@ -1,48 +1,237 @@
-const Yeelight = require('yeelight2');
+class sYeelight {
+  constructor() {
+    this.yeelight = require('yeelight2');
 
-Yeelight.discover(function(light){
+    this.light = this.yeelight.discover(function (light) {
 
-  console.log(light.name);
+        console.log(light.name);
+        light.set_power(false, undefined, undefined);
+      });
+  }
 
-  function flip(){
+  turn_on() {
+    this.light.set_power(true, undefined, undefined);
+  }
+
+  turn_off() {
+    this.light.set_power(false, undefined, undefined);
+  }
+
+  flip(light) {
     light.toggle();
   }
 
-  function turn_on(){
-    light.set_power(true, undefined, undefined);
-  }
-
-  function turn_off() {
-    light.set_power(false, undefined, undefined);
-  }
-
-  function max_red(){
-    light.set_bright(100, undefined, undefined);
+  max_red(light) {
     light.set_ct_abx(2700, undefined, undefined);
   }
 
-  function max_green(){
-    light.set_bright(100, undefined, undefined);
+  max_green(light) {
     light.set_ct_abx(6500, undefined, undefined);
   }
 
-  function percent_red(percent) {
-    percent_green(100 - percent);
-  }
-
-  function percent_green(percent) {
+  percent_green(light, percent) {
     // values should be between 2700 and 6500
     const ct_value = 2700 + (6500 - 2700) / 100 * percent;
     //const ct_value = 2700 + 38 * percent;
     light.set_ct_abx(ct_value, undefined, undefined);
   }
 
+  percent_red(light, percent) {
+    percent_green(100 - percent);
+  }
 
-  turn_on();
-  percent_red(100);
-  // percent_red(50);
-  // percent_red(0);
+  set_brightness(brightness) {
+    light.set_bright(brightness, undefined, undefined);
+  }
+}
 
-  //setInterval(blink, 2000);
+// yeelight = new Yeelight();
+// test = function() {
+//  console.log(yeelight);
+// };
+//
+// setTimeout(test, 10000);
 
-});
+let Yeelight = require('yeelight2');
+
+
+function sleep(time){
+  time = time || 3000;
+  return function(o){
+    return new Promise(fn => setTimeout(() => fn(o), time));
+  };
+}
+
+Promise.resolve()
+  .then(() => new Promise(accept => {
+    Yeelight.discover(function(light){
+      this.close();
+      accept(light);
+    });
+  }))
+  .then(light => {
+    light // get name
+      .get_prop('name')
+      .then(response => console.log('Your bulb name is: [%s]', response.name))
+    return light;
+  })
+  .then(light => {
+    light // turn on light
+      .set_power('on')
+      .then(response => console.log('turn on light succeed'))
+    return light;
+  })
+  .then(light => {
+    light // set color to white
+      .set_rgb(0xffffff)
+      .then(response => console.log('set color to white(0xffffff) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set brightness to 30%
+      .set_bright(30)
+      .then(response => console.log('set brightness to 30% succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light
+      .toggle()
+      .then(response => console.log('toggle succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light
+      .toggle()
+      .then(response => console.log('toggle succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set brightness to 100%
+      .set_bright(100)
+      .then(response => console.log('set brightness to 100% succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set color to red
+      .set_rgb(0xff0000)
+      .then(response => console.log('set color to red(0xff0000) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set color to green
+      .set_rgb(0x00ff00)
+      .then(response => console.log('set color to green(0x00ff00) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set color to blue
+      .set_rgb(0x0000ff)
+      .then(response => console.log('set color to blue(0x0000ff) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // turn off light
+      .set_power('off')
+      .then(response => console.log('turn off light succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light.exit();
+    console.log('Bye! have a nice day :)');
+  });
+
+Promise.resolve()
+  .then(() => new Promise(accept => {
+    Yeelight.discover(function(light){
+      this.close();
+      accept(light);
+    });
+  }))
+  .then(light => {
+    light // get name
+      .get_prop('name')
+      .then(response => console.log('Your bulb name is: [%s]', response.name))
+    return light;
+  })
+  .then(light => {
+    light // turn on light
+      .set_power('on')
+      .then(response => console.log('turn on light succeed'))
+    return light;
+  })
+  .then(light => {
+    light // set color to white
+      .set_rgb(0xffffff)
+      .then(response => console.log('set color to white(0xffffff) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set brightness to 30%
+      .set_bright(30)
+      .then(response => console.log('set brightness to 30% succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light
+      .toggle()
+      .then(response => console.log('toggle succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light
+      .toggle()
+      .then(response => console.log('toggle succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set brightness to 100%
+      .set_bright(100)
+      .then(response => console.log('set brightness to 100% succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set color to red
+      .set_rgb(0xff0000)
+      .then(response => console.log('set color to red(0xff0000) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set color to green
+      .set_rgb(0x00ff00)
+      .then(response => console.log('set color to green(0x00ff00) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // set color to blue
+      .set_rgb(0x0000ff)
+      .then(response => console.log('set color to blue(0x0000ff) succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light // turn off light
+      .set_power('off')
+      .then(response => console.log('turn off light succeed'))
+    return light;
+  })
+  .then(sleep())
+  .then(light => {
+    light.exit();
+    console.log('Bye! have a nice day :)');
+  });
